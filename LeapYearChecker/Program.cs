5811e62bcc5acc2312e0d5e488b97946a3cf951f
Gregory Otokhina <gregory.otokhina@magicsubcriptions.com>
2022-07-01 13:30:24 +0100

    first commit

---
+++ LeapYearChecker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LeapYearChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var year = int.Parse(args[0]);
+            Console.WriteLine(IsLeapYear(year));
+        }
+
+        private static bool IsLeapYear(int year)
+        {
+            return (year % 100 != 0 || year % 400 == 0) && year % 4 == 0;
+        }
+    }
+}
+
